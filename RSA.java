9eeb186c1d903dbc2a7cfbc50af9d719f0191788
RosaPavan <127827165+RosaPavan@users.noreply.github.com>
2024-11-18 02:30:34 +0100

    RSA.java

--- RSA.java
+++ RSA.java
@@ -1,38 +1,75 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class RSA {
-    public static void main(String[] args) throws Exception {
-        Scanner parola = new Scanner(System.in);
-        System.out.println("Scrivi una parola:");
-        String userName = parola.nextLine();
+        Scanner inp = new Scanner(System.in);
+        String parola = inp.nextLine();
+    public static void main(String[] args){
         encryption();
     }
     private static void encryption(){	
 		Random rnd = new Random();
 		BigInteger p = BigInteger.probablePrime(Long.BYTES, rnd);
         BigInteger q = BigInteger.probablePrime(Long.BYTES, rnd);
-        BigInteger fEulero = p.subtract(BigInteger.ONE).multiply((q.subtract(BigInteger.ONE)));
-        BigInteger e;
-		while ((e = BigInteger.probablePrime(Long.BYTES, rnd)).compareTo(fEulero)>0) {
-		}
         if (p.gcd(q) != BigInteger.ONE) {
             p = BigInteger.probablePrime(Long.BYTES, rnd);
             q = BigInteger.probablePrime(Long.BYTES, rnd);
         }
-
+        BigInteger fEulero = p.subtract(BigInteger.ONE).multiply((q.subtract(BigInteger.ONE)));
+        BigInteger e = generateE(fEulero, rnd);
         BigInteger n = p.multiply(q);
-
+        BigInteger d;
+        BigInteger i = BigInteger.ZERO;
         while(true){
-            BigInteger i;
-            BigInteger x = BigInteger.ONE.add(fEulero.multiply(i));
-            if (x % e == 0) {
-                d = x / e;
+            BigInteger a = BigInteger.ONE.add(fEulero.multiply(i));
+            if (a.mod(e) == BigInteger.ZERO) {
+                d = a.divide(e);
                 break;
             }
-            i = i.add(BigInteger.ONE);
+            i.add(BigInteger.ONE);
         }
+        String parola = "ciao";
+        ArrayList<Integer> converted = convert(parola);
+        System.out.println("Messaggio convertito: " + converted);    
+        ArrayList<BigInteger> encrypted = encrypt(converted, e, n);
+        System.out.println("Messaggio cifrato: " + encrypted);
+        ArrayList<Character> decrypted = decrypt(encrypted, d, n);
+        System.out.println("Messaggio decifrato: " + decrypted);
+    }
+    private static BigInteger generateE(BigInteger fEulero, Random rnd) {
+        BigInteger e;
+        do {
+            e = new BigInteger(512, rnd);
+        } while (e.compareTo(BigInteger.ONE) <= 0 || e.compareTo(fEulero) >= 0 || !e.gcd(fEulero).equals(BigInteger.ONE));
+        return e;
+    }
+    public static ArrayList<Integer> convert(String parola){
+        ArrayList<Integer> converted = new ArrayList<Integer>();
+        for (int i = 0; i < parola.length(); i++) {
+            char lettera = parola.charAt(i);
+            int x = lettera;
+            converted.add(x);
+        }
+        return (converted);
+    }
+    private static ArrayList<BigInteger> encrypt(ArrayList<Integer> data, BigInteger e, BigInteger n) {
+        ArrayList<BigInteger> encryptedValues = new ArrayList<BigInteger>();
+        for (Integer value : data) {
+            BigInteger m = BigInteger.valueOf(value);
+            BigInteger cipher = m.modPow(e, n);
+            encryptedValues.add(cipher);
+        }
+        return encryptedValues;
+    }
+    public static ArrayList<Character> decrypt(ArrayList<BigInteger> encryptedData, BigInteger d, BigInteger n) {
+        ArrayList<Character> decryptedValues = new ArrayList<Character>();
+        for (BigInteger cipheredValue : encryptedData) {
+            BigInteger decryptedValue = cipheredValue.modPow(d, n);
+            decryptedValues.add((char) decryptedValue.intValue());
+        }
+        return decryptedValues;
     }
 }
